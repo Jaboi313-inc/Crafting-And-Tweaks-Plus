6b5bec5315ae5edd16ab55b950cb6c8b5e4c8c7a
jaboi313 <hollanderkelvin@gmail.com>
2026-07-25 15:53:57 +0200

    Made config options mod dependant

--- src/client/java/com/jaboi313/craftingandtweaksplus/ConfigScreen.java
+++ src/client/java/com/jaboi313/craftingandtweaksplus/ConfigScreen.java
@@ -1,16 +1,22 @@
 package com.jaboi313.craftingandtweaksplus;
 
+import java.net.URI;
+
 import com.jaboi313.craftingandtweaksplus.config.ConfigManager;
 
 import me.shedaniel.clothconfig2.api.ConfigBuilder;
 import me.shedaniel.clothconfig2.api.ConfigCategory;
-
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.ClickEvent;
 import net.minecraft.network.chat.Component;
 
 public class ConfigScreen {
 
     public static Screen create(Screen parent) {
+
+        boolean progressionLoaded = FabricLoader.getInstance()
+        .isModLoaded("progression-plus");
 
         ConfigBuilder builder = ConfigBuilder.create()
                 .setParentScreen(parent)
@@ -18,18 +24,27 @@ public class ConfigScreen {
 
 
         ConfigCategory harness = builder.getOrCreateCategory(
-                Component.literal("Harness")
+                Component.literal("Speed Harness")
         );
 
 
         harness.addEntry(
                 builder.entryBuilder()
                         .startTextDescription(
-                                Component.literal(
-                                        "Speed values are percentages.\n" +
-                                        "100 = +100% flying speed (2x total speed).\n" +
-                                        "Changes require a game restart to take effect!"
-                                )
+                                Component.literal("Speed values are always added to the default speed.\n")
+                                        .withStyle(style -> style.withColor(0xFFFFFF))
+                                        .append(
+                                                Component.literal("0 = default speed.\n")
+                                                        .withStyle(style -> style.withColor(0x55FF55))
+                                        )
+                                        .append(
+                                                Component.literal("100 = +100% flying speed (2x total speed).\n")
+                                                        .withStyle(style -> style.withColor(0x55FF55))
+                                        )
+                                        .append(
+                                                Component.literal("Changes require a game restart to take effect!")
+                                                        .withStyle(style -> style.withColor(0xFF5555))
+                                        )
                         )
                         .build()
         );
@@ -43,13 +58,40 @@ public class ConfigScreen {
                         )
                         .setDefaultValue(25)
                         .setMin(1)
-                        .setMax(1000)
+                        .setMax(10000)
+                        .requireRestart()
                         .setSaveConsumer(value -> {
                             ConfigManager.config.harness.speedLevel1 = value;
                             ConfigManager.save();
                         })
                         .build()
         );
+
+        if (!progressionLoaded) {
+
+                harness.addEntry(
+                        builder.entryBuilder()
+                                .startTextDescription(
+                                        Component.literal("Progression Plus is required for higher speed levels.\n")
+                                                .withStyle(style -> style.withColor(0xFF5555))
+                                                .append(
+                                                        Component.literal("Download it here")
+                                                                .withStyle(style -> style
+                                                                        .withColor(0x55FFFF)
+                                                                        .withUnderlined(true)
+                                                                        .withClickEvent(
+                                                                                new ClickEvent.OpenUrl(
+                                                                                        URI.create(
+                                                                                        "https://modrinth.com/mod/progressionplusmod"
+                                                                                        )
+                                                                                )
+                                                                        )
+                                                                )
+                                                )
+                                )
+                                .build()
+                );
+                }
 
 
         harness.addEntry(
@@ -60,7 +102,9 @@ public class ConfigScreen {
                         )
                         .setDefaultValue(50)
                         .setMin(1)
-                        .setMax(1000)
+                        .setMax(10000)
+                        .requireRestart()
+                        .setRequirement(() -> progressionLoaded)
                         .setSaveConsumer(value -> {
                             ConfigManager.config.harness.speedLevel2 = value;
                             ConfigManager.save();
@@ -77,7 +121,9 @@ public class ConfigScreen {
                         )
                         .setDefaultValue(75)
                         .setMin(1)
-                        .setMax(1000)
+                        .setMax(10000)
+                        .requireRestart()
+                        .setRequirement(() -> progressionLoaded)
                         .setSaveConsumer(value -> {
                             ConfigManager.config.harness.speedLevel3 = value;
                             ConfigManager.save();
@@ -94,7 +140,9 @@ public class ConfigScreen {
                         )
                         .setDefaultValue(100)
                         .setMin(1)
-                        .setMax(1000)
+                        .setMax(10000)
+                        .requireRestart()
+                        .setRequirement(() -> progressionLoaded)
                         .setSaveConsumer(value -> {
                             ConfigManager.config.harness.speedLevel4 = value;
                             ConfigManager.save();
@@ -111,7 +159,9 @@ public class ConfigScreen {
                         )
                         .setDefaultValue(150)
                         .setMin(1)
-                        .setMax(1000)
+                        .setMax(10000)
+                        .requireRestart()
+                        .setRequirement(() -> progressionLoaded)
                         .setSaveConsumer(value -> {
                             ConfigManager.config.harness.speedLevel5 = value;
                             ConfigManager.save();
